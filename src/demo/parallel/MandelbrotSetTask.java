5763b0cbec9db4912a0840d4532790b7f90687c4
KabacheuskyDmitry <dimmmon4@gmail.com>
2024-10-29 23:05:13 +0300

    gamma

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -31,29 +31,15 @@
 package demo.parallel;
 
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 import javafx.concurrent.Task;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
-/**
- * Task to render Mandelbrot set using given parameters. See {@link 
- * #MandelbrotRendererTask(boolean, javafx.scene.image.PixelWriter, int, int, 
- * double, double, double, double, double, double, double, double, boolean) 
- * constructor} for parameters list. The task returns time in milliseconds as 
- * its calculated value.
- * 
- * <p><i>
- * This source code is provided to illustrate the usage of a given feature
- * or technique and has been deliberately simplified. Additional steps
- * required for a production-quality application, such as security checks,
- * input validation and proper error handling, might not be present in
- * this sample code.</i>
- * 
- * @author Alexander Kouznetsov, Tristan Yan
- */
+
+
 class MandelbrotSetTask extends Task<Long> {
     
     /**
@@ -337,7 +323,12 @@ class MandelbrotSetTask extends Task<Long> {
         if (count >= colors.length) {
             return Color.BLACK;
         }
-        return colors[count];
+        double gamma = 0.5; // Измените значение, чтобы настроить гамму
+        double normalizedCount = (double) count / (colors.length - 1);
+        double correctedValue = Math.pow(normalizedCount, gamma);
+
+        int index = (int) (correctedValue * (colors.length - 1));
+        return colors[index];
     }
     
     /**
